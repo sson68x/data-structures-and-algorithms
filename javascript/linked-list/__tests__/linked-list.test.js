4819d4d137a143e345868b029afb4646df7cb011
Seungwoo Son <sson68x@gmail.com>
2022-06-27 22:53:14 -0700

    CC5 completed

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,57 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const { LinkedList, Node } = require('../index');
+
 
 describe('Linked List', () => {
   it('works', () => {
     expect(true).toBeTruthy();
+  });
+
+  let list = new LinkedList();
+
+  test('Can successfully instantiate an empty linked list', () => {
+    expect(list.head).toBeNull;
+  });
+
+  test('Can properly insert into the linked list', () => {
+    list.insert(3);
+
+    expect(list.head.value).toEqual(3);
+    expect(list.head.next).toBeNull();
+  });
+
+  test('The head property will properly point to the first node in the linked list', () => {
+    let expected = list.head.value;
+    expect(list.head.value).toEqual(expected);
+  });
+
+  test('Can properly insert multiple nodes into the linked list', () => {
+    list.insert(5);
+    list.insert(7)
+
+    expect(list.head.value).toEqual(7);
+    expect(list.head.next.value).toEqual(5);
+    expect(list.head.next.next.value).toEqual(3);
+    expect(list.head.next.next.next).toBeNull;
+  });
+
+  test('Will return true when finding a value within the linked list that exists', () => {
+    let result = list.includes(3);
+    expect(result).toBeTruthy();
+  });
+
+  test('Will return false when searching for a value in the linked list that does not exist', () => {
+    let anotherResult = list.includes('not included');
+
+    expect(anotherResult).toBeFalsy();
+  });
+
+  test('Can properly return a collection of all the values that exist in the linked list', () => {
+    let str = list.toString();
+    let expected = '{ 7 } -> { 5 } -> { 3 } -> NULL';
+
+    expect(str).toEqual(expected);
   });
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,58 @@
 'use strict';
 
-class LinkedList {
-
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-module.exports = LinkedList;
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  traverse() {
+    let current = this.head;
+    while (current) {
+      current = current.next;
+    }
+    console.log('Done traversing');
+  }
+
+  // Code Challenge 5 - Linked List
+
+  insert(value) {
+    let newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  includes(val) {
+    let current = this.head;
+    while (current) {
+      if (current.value === val) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  toString() {
+    let current = this.head;
+    let result = '';
+    while (current) {
+      result += `{ ${current.value} } -> `;
+      current = current.next;
+    }
+
+    result += 'NULL';
+    return result;
+  };
+};
+
+module.exports = {
+  Node,
+  LinkedList,
+};
