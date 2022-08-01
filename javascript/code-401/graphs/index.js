a125d4061a41302e9db6f2dcc7c1aebc283a1290
Seungwoo Son <sson68x@gmail.com>
2022-07-31 22:51:35 -0700

    CC35 completed

---
+++ javascript/code-401/graphs/index.js
@@ -0,0 +1,124 @@
+' use strict';
+
+class Vertex {
+  constructor(value) {
+    this.value = value;
+  }
+}
+
+class Edge {
+  constructor(vertex, weight = 0) {
+    this.vertex = vertex;
+    this.weight = weight;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.adjacencyList = new Map();
+  }
+
+  addVertex(value) {
+    const vertex = new Vertex(value);
+    this.adjacencyList.set(vertex, []);
+    return vertex;
+  }
+
+  // find the node we want to connect
+  addDirectedEdge(startVertex, endVertex) {
+    const neighbors = this.adjacencyList.get(startVertex);
+    neighbors.push(new Edge(endVertex));
+  }
+
+  getNodes() {
+    return [this.adjacencyList.keys()];
+  }
+
+  getNeighbors(vertex) {
+    return [...this.adjacencyList.get(vertex)];
+  }
+
+  graphSize() {
+    return [...this.adjacencyList].length;
+  }
+
+  breadthFirst(root, cb) {
+    const queue = [root];
+    const visited = new Set();
+    visited.add(root);
+    let current = null;
+
+    while (queue.length) {
+      current = queue.pop();
+
+      // do something
+      if (cb) cb(current.value);
+
+      // grab neighbors?
+      const neighbors = this.getNeighbors(current);
+      for (let edge of neighbors) {
+        if (!visited.has(edge.vertex)) {
+          visited.add(edge.vertex);
+          queue.unshift(edge.vertex);
+        }
+      }
+    }
+    return visited;
+  }
+
+  depthFirst(root, cb) {
+    const stack = [root];
+    const visited = new Set();
+    visited.add(root);
+    let current = null;
+
+    while (stack.length) {
+      current = stack.pop();
+
+      // do something if we want
+      if (cb) cb(current.value);
+
+      // grab neighbors?
+      const neighbors = this.getNeighbors(current);
+      for (let edge of neighbors) {
+        if (!visited.has(edge.vertex)) {
+          visited.add(edge.vertex);
+          stack.push(edge.vertex);
+        }
+      }
+    }
+    return visited;
+  }
+
+}
+
+const graph = new Graph();
+
+const A = graph.addVertex('A');
+const B = graph.addVertex('B');
+const D = graph.addVertex('D');
+const C = graph.addVertex('C');
+const E = graph.addVertex('E');
+const F = graph.addVertex('F');
+const G = graph.addVertex('G');
+const H = graph.addVertex('H');
+
+graph.addDirectedEdge(A, B);
+graph.addDirectedEdge(A, D);
+graph.addDirectedEdge(A, C);
+graph.addDirectedEdge(B, G);
+graph.addDirectedEdge(D, F);
+graph.addDirectedEdge(D, H);
+graph.addDirectedEdge(F, H);
+graph.addDirectedEdge(C, H);
+graph.addDirectedEdge(F, E);
+
+// console.log(graph.adjacencyList);
+// graph.breadthFirst(A, console.log);
+// graph.depthFirst(A, console.log);
+
+module.exports = {
+  Vertex,
+  Edge,
+  Graph
+};
